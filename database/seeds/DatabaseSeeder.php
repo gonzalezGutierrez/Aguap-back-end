bc23cb7f0758e94f3638cf48947d27edd4ff8c41
PC <alexisperezgomez1@gmial.com>
2020-12-04 22:43:34 -0600

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            RolTableSeed::class
+        DB::table('tbl_roles')->insert([
+            'rol'=>"administrador",
+        ]);
+        
+        DB::table('tbl_roles')->insert([
+            'rol'=>"repartidor",
+        ]);
+
+        DB::table('tbl_roles')->insert([
+            'rol'=>"cliente",
         ]);
 
         factory(\App\User::class)->times(200)->create();
@@ -23,9 +31,39 @@ class DatabaseSeeder extends Seeder
         /*factory(\App\Order::class)->times(80)->create();
         factory(\App\ServicioOrder::class)->times(150)->create();*/
 
-        $this->call([
-            OrderSeeder::class,
-            ServicioOrderSeeder::class
-        ]);
+        $usuarios = \App\User::all();
+        $ubicaciones = \App\Ubication::all();
+        for ($i = 0; $i<80; $i++) {
+
+            $fechaInicial = "2020-01-01";
+            $fechaFinal   = "2020-12-31";
+
+            $milisegundosLimiteInferior = strtotime($fechaInicial);
+            $milisegundosLimiteSuperior = strtotime($fechaFinal);
+
+            $milisegundosAleatorios = mt_rand($milisegundosLimiteInferior,$milisegundosLimiteSuperior);
+
+            $fecha = date('Y-m-d',$milisegundosAleatorios);
+
+
+            $order = new \App\Order();
+            $order->idCliente = $usuarios->random()->idUsuario;
+            $order->idRepartidor = $usuarios->random()->idUsuario;
+            $order->idUbicacion  = $ubicaciones->random()->idUbicacion;
+            $order->fechaOrden   = $fecha;
+            $order->estatus = 1;
+            $order->save();
+        }
+        $ordenes = \App\Order::all();
+        $servicios = \App\CatServicio::all();
+
+        for($i= 0; $i<150; $i++) {
+            $servicioOrden = new \App\ServicioOrder();
+            $servicioOrden->idOrden = $ordenes->random()->idOrden;
+            $servicioOrden->idServicio = $servicios->random()->idServicio;
+            $servicioOrden->cantidad   = rand(0,15);
+            $servicioOrden->subtotal   = $servicioOrden->cantidad * rand(0,100);
+            $servicioOrden->save();
+        }
     }
 }
