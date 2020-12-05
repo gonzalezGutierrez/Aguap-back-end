6046975ca6f89ab461331c2f272683a1edb06a2b
SalvadorLG <153192@ids.upchiapas.edu.mx>
2020-12-04 19:04:19 -0600

    insumos y proveedores API

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('v1/register', 'UserController@store');
 Route::get('v1/user/email','UserController@findEmail');
 
+    Route::post('v1/supplie/', 'SuppliesController@store');
+    Route::post('v1/supplier/', 'SuppliersController@store');
+
+    Route::get('v1/supplie/','SuppliesController@index');
+    Route::get('v1/supplier/','SuppliersController@index');
+
+    Route::get('v1/supplie/{id}','SuppliesController@show');
+    Route::get('v1/supplier/{id}','SuppliersController@show');
+
+    Route::delete('v1/supplie/{id}','SuppliesController@destroy');
+    Route::delete('v1/supplier/{id}','SuppliersController@destroy');
+
+    Route::put('v1/supplie/{id}','SuppliesController@update');
+    Route::put('v1/supplier/{id}','SuppliersController@update');
+    
 Route::middleware(['middleware'=>'auth:api'])->group(function () {
     Route::get('v1/user/{id}','UserController@show');
     Route::put('v1/user/{id}','UserController@update');
     Route::post('v1/ubication','UbicationController@store');
     Route::get('v1/ubication','UbicationController@index');
     Route::delete('v1/ubication/{id}','UbicationController@destroy');
+
+    
 });
