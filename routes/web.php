df7585c085eac71523d0c640f97b58cacae9f60c
SalvadorLG <153192@ids.upchiapas.edu.mx>
2020-12-09 13:51:47 -0600

    CRUD proveedores e insumos

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('email');
 });
+Route::resource('_proveedores','\App\Http\Controllers\SuppliersController');
+Route::post('_update_proveedor','\App\Http\Controllers\SuppliersController@actualizar')->name('_update_proveedor.actualizar');
 
+Route::resource('_insumos','\App\Http\Controllers\SuppliesController');
+Route::post('_update_insumos','\App\Http\Controllers\SuppliesController@actualizar')->name('_update_insumos.actualizar');
 
 Route::group(['prefix'=>'administracion'],function(){
     Route::get('pedidos',[\App\Http\Controllers\PedidosController::class,'index']);
+    Route::get('proveedores',[\App\Http\Controllers\SuppliersController::class, 'index']);
+    Route::get('insumos',[\App\Http\Controllers\SuppliesController::class, 'index']);
 });
